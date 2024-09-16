af3b7240ff1d36cce7fe32cca3447344b2983b1d
coji <coji@techtalk.jp>
2024-09-16 16:00:59 +0900

    Add Stack and HStack components for UI layout

--- app/routes/_index/form.test.tsx
+++ app/routes/_index/form.test.tsx
@@ -15,11 +15,17 @@ test('Form', async () => {
 
   render(<RemixStub />);
 
-  await userEvent.type(screen.getByLabelText('Email'), 'coji@techtalk.jp');
-  await userEvent.type(screen.getByLabelText('Name'), 'Coji');
-  await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+  await userEvent.type(screen.getByLabelText('お名前'), '溝口浩二');
+  await userEvent.type(screen.getByLabelText('会社名'), '株式会社TechTalk');
+  await userEvent.type(screen.getByLabelText('電話番号'), '09012345678');
+  await userEvent.type(screen.getByLabelText('メール'), 'coji@techtalk.jp');
+  await userEvent.type(screen.getByLabelText('メッセージ'), 'こんにち\nは');
+  await userEvent.click(screen.getByRole('checkbox', { name: 'privacy' }));
+  await userEvent.click(screen.getByRole('button', { name: "Let's talk" }));
 
   expect(
-    await screen.findByText('Thank you', { exact: false })
+    await screen.findByText('お問い合わせありがとうございます', {
+      exact: false,
+    })
   ).toBeInTheDocument();
 });
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,19 +1,46 @@
-import { Form, useActionData } from '@remix-run/react';
+import { Form, useActionData, useFetcher } from '@remix-run/react';
 import { z } from 'zod';
-import { parseWithZod } from '@conform-to/zod';
-import { getFormProps, getInputProps, useForm } from '@conform-to/react';
+import { parseWithZod, getZodConstraint } from '@conform-to/zod';
+import {
+  getFormProps,
+  getInputProps,
+  getTextareaProps,
+  useForm,
+} from '@conform-to/react';
 import type { ActionFunctionArgs } from '@remix-run/node';
 import { setTimeout } from 'node:timers/promises';
 import { Button } from '~/components/ui/button';
 import { Label } from '~/components/ui/label';
+import { Textarea } from '~/components/ui/textarea';
 import { Input } from '~/components/ui/input';
+import { Stack, HStack } from '~/components/ui/stack';
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert';
+import { Checkbox } from '~/components/ui/checkbox';
+import {
+  Card,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '~/components/ui/card';
+
+export const contactFormSchema = z.object({
+  name: z.string().max(100),
+  company: z.string().max(100).optional(),
+  phone: z.string().max(20).optional(),
+  email: z.string().max(100).email(),
+  message: z.string().max(10000),
+  privacyPolicy: z.string().transform((value) => value === 'on'),
+});
+
+type ContactFormData = z.infer<typeof contactFormSchema>;
 
 const schema = z.discriminatedUnion('intent', [
-  z.object({
-    intent: z.literal('submit'),
-    email: z.string().email(),
-    name: z.string(),
-  }),
+  z
+    .object({
+      intent: z.literal('submit'),
+    })
+    .merge(contactFormSchema),
   z.object({
     intent: z.literal('reset'),
   }),
@@ -37,49 +64,158 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   };
 };
 
-export default function Index() {
-  const actionData = useActionData<typeof action>();
-  const [form, { email, name }] = useForm({
-    lastResult: actionData?.lastResult,
-    onValidate: ({ formData }) => parseWithZod(formData, { schema }),
-  });
+export const ContactSentMessage = ({ data }: { data: ContactFormData }) => {
+  const fetcher = useFetcher<typeof action>({ key: 'contact' });
+  return (
+    <Card>
+      <CardContent>
+        <CardTitle>Message has been sent</CardTitle>
+        <CardDescription>
+          お問い合わせありがとうございます。 以下のメッセージを受付けました。
+          お返事をお待ち下さい。
+        </CardDescription>
+        <div className="grid max-h-96 w-full max-w-md grid-cols-[auto_1fr] justify-items-start gap-4 overflow-auto rounded p-4">
+          <div>お名前</div>
+          <div>{data.name}</div>
+          <div>会社名</div>
+          <div>{data.company}</div>
+          <div>電話番号</div>
+          <div>{data.phone}</div>
+          <div>メールアドレス</div>
+          <div>{data.email}</div>
+          <div>メッセージ</div>
+          <div>{data.message}</div>
+        </div>
 
-  const isSubmitted = actionData?.value;
-  if (isSubmitted) {
-    return (
-      <div>
-        <h1>Thank you!</h1>
-        <pre>{JSON.stringify(isSubmitted, null, 2)}</pre>
-        <form method="POST">
-          <Button name="intent" value="reset" type="submit">
-            Submit Another
-          </Button>
-        </form>
-      </div>
-    );
+        <CardFooter>
+          <fetcher.Form method="POST">
+            <Button
+              type="submit"
+              name="intent"
+              value="reset"
+              disabled={fetcher.state === 'submitting'}
+            >
+              OK
+            </Button>
+          </fetcher.Form>
+        </CardFooter>
+      </CardContent>
+    </Card>
+  );
+};
+
+type ContactFormProps = React.HTMLAttributes<HTMLFormElement>;
+export const ContactForm = ({ children, ...rest }: ContactFormProps) => {
+  const fetcher = useFetcher<typeof action>({ key: 'contact' });
+  const actionData = fetcher.data;
+  const [form, { name, company, phone, email, message, privacyPolicy }] =
+    useForm({
+      lastResult: actionData?.lastResult,
+      constraint: getZodConstraint(schema),
+      onValidate: ({ formData }) => parseWithZod(formData, { schema }),
+      shouldValidate: 'onSubmit',
+      shouldRevalidate: 'onInput',
+    });
+
+  if (actionData?.value?.intent === 'submit') {
+    return <ContactSentMessage data={actionData.value} />;
   }
 
   return (
-    <Form method="POST" {...getFormProps(form)}>
-      <div>
-        <Label htmlFor={email.id}>Email</Label>
-        <Input {...getInputProps(email, { type: 'text' })} />
-        <div id={email.errorId} className="text-destructive text-sm">
-          {email.errors}
+    <fetcher.Form method="POST" {...rest} {...getFormProps(form)}>
+      <Stack>
+        <div>
+          <Label htmlFor={name.id}>お名前</Label>
+          <Input
+            autoComplete="name"
+            {...getInputProps(name, { type: 'text' })}
+          />
+          <div id={name.errorId} className="text-red-500">
+            {name.errors}
+          </div>
         </div>
-      </div>
 
-      <div>
-        <Label htmlFor={name.id}>Name</Label>
-        <Input {...getInputProps(name, { type: 'text' })} />
-        <div id={name.errorId} className="text-destructive text-sm">
-          {name.errors}
+        <div>
+          <Label htmlFor={company.id}>会社名</Label>
+          <Input
+            autoComplete="organization"
+            {...getInputProps(company, { type: 'text' })}
+          />
+          <div id={company.errorId} className="text-red-500">
+            {company.errors}
+          </div>
         </div>
-      </div>
 
-      <Button name="intent" value="submit" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <div>
+          <Label htmlFor={phone.id}>電話番号</Label>
+          <Input
+            autoComplete="tel"
+            {...getInputProps(phone, { type: 'tel' })}
+          />
+          <div id={phone.errorId} className="text-red-500">
+            {phone.errors}
+          </div>
+        </div>
+
+        <div>
+          <Label htmlFor={email.id}>メール</Label>
+          <Input
+            autoComplete="email"
+            {...getInputProps(email, { type: 'email' })}
+          />
+          <div id={email.errorId} className="text-red-500">
+            {email.errors}
+          </div>
+        </div>
+
+        <div>
+          <Label htmlFor={message.id}>メッセージ</Label>
+          <Textarea autoComplete="off" {...getTextareaProps(message)} />
+          <div id={message.errorId} className="text-red-500">
+            {message.errors}
+          </div>
+        </div>
+
+        <div>
+          <HStack className="items-center">
+            <Checkbox
+              id={privacyPolicy.id}
+              name={privacyPolicy.name}
+              aria-invalid={privacyPolicy.errors ? true : undefined}
+              aria-describedby={
+                privacyPolicy.errors ? privacyPolicy.errorId : undefined
+              }
+              defaultChecked={privacyPolicy.initialValue === 'on'}
+              aria-label="privacy"
+            />
+            <label htmlFor={privacyPolicy.id} className="cursor-pointer">
+              プライバシーポリシーに同意する
+            </label>
+          </HStack>
+          <div id={privacyPolicy.errorId} className="text-red-500">
+            {privacyPolicy.errors}
+          </div>
+        </div>
+
+        {form.errors && (
+          <Alert variant="destructive">
+            <AlertTitle>System Error</AlertTitle>
+            <AlertDescription>{form.errors}</AlertDescription>
+          </Alert>
+        )}
+
+        <Button
+          type="submit"
+          name="intent"
+          value="submit"
+          disabled={fetcher.state === 'submitting'}
+        >
+          Let's talk
+        </Button>
+
+        <div>{JSON.stringify(form.allErrors)}</div>
+      </Stack>
+    </fetcher.Form>
   );
-}
+};
+export default ContactForm;
